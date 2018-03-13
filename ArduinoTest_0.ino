fc2793937127d424ba7449c73505c0f70f16be62
ChunXi1999 Fang <fangchunxi19990113@gmail.com>
2018-03-13 13:11:15 +0700

    1

--- ArduinoTest_0.ino
+++ ArduinoTest_0.ino
@@ -17,7 +17,8 @@
 #define intp 2
 
 bool is_intp = true;
-byte modeNum = 0;
+byte modeNum = -1;
+byte modeMax = 3;
 int segLight = 255;
 
 void setup()
@@ -40,36 +41,102 @@ void setup()
 
 	pinMode(intp, INPUT_PULLUP);
 
-	attachInterrupt(digitalPinToInterrupt(intp), intp_program, LOW);
+	attachInterrupt(digitalPinToInterrupt(intp), intp_program, CHANGE);
 
 	checkSystem();
 }
 
 void loop()
 {
-	is_intp = false;
+	segLight = bit10Tobit8(checkA6());
+	bool is_onDP = false;
+	float var = 0;
 
-	segLight = bit10Tobit8(checkA7());
+	/*
+	if (is_intp == true)
+	{
+		offLED();
+		delay(100);
+		analogWrite(dig3, segLight);
+		segDisplay(modeNum % modeMax);
+		is_intp = false;
+		delay(395);
+		offLED();
+	}*/
+
+
+	switch (modeNum % modeMax)
+	{
+		case 0:
+			var = 0.0f;
+			is_onDP = false;
+			break;
+		case 1:
+			var = 1.1f;
+			is_onDP = true;
+			break;
+		case 2:
+			var = 2.2f;
+			is_onDP = true;
+			break;
+
+		default:
+			var = 5.0f;
+			is_onDP = false;
+			break;
+	}
+
+	//display
+
+	var *= 100.0f;
+
+	int t1 = var / 1000;
+	var = var - t1 *1000;
+	offLED();
+	analogWrite(dig1, segLight);
+	segDisplay(t1);
+
+	int t2 = var / 100;
+	var = var - t2 *100;
+	offLED();
+	analogWrite(dig2, segLight);
+	if (is_onDP)
+	{
+		digitalWrite(segDP, HIGH);
+	}
+	segDisplay(t2);
+
+	int t3 = var / 10;
+	var = var - t3 *10;
+	offLED();
+	analogWrite(dig3, segLight);
+	segDisplay(t3);
+
+	int t4 = var;
+	offLED();
+	analogWrite(dig4, segLight);
+	segDisplay(t4);
+	//display
+
 	Serial.print("modeNum: ");
 	Serial.println(modeNum);
-	
+	Serial.print("segLight: ");
+	Serial.println(segLight);
+	is_intp = false;
 }
 
 void intp_program()
 {
-	if (!is_intp)
+	if (digitalRead(intp) == HIGH)
 	{
 		modeNum++;
-		setAlldig(HIGH);
-		digitalWrite(segG, HIGH);
-		offLED();
-		is_intp = true;
 	}
+	is_intp = true;
 }
 
 int bit10Tobit8(int bit10)
 {
-	return (bit10 / 1023) * 255;
+	return (bit10 * 256) / 1024;
 }
 
 int checkA7()
@@ -81,6 +148,102 @@ int checkA6()
 {
 	return analogRead(A6);
 }
+
+void segDisplay(int c)
+{
+    switch (c)
+    {
+        case 0:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segB, HIGH);
+			digitalWrite(segC, HIGH);
+			digitalWrite(segD, HIGH);
+			digitalWrite(segE, HIGH);
+			digitalWrite(segF, HIGH);
+			delay(5);
+            break;
+        
+        case 1:
+            digitalWrite(segB, HIGH);
+			digitalWrite(segC, HIGH);
+			delay(5);
+            break;
+
+        case 2:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segB, HIGH);
+			digitalWrite(segD, HIGH);
+			digitalWrite(segE, HIGH);
+			digitalWrite(segG, HIGH);
+			delay(5);
+            break;
+
+        case 3:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segB, HIGH);
+			digitalWrite(segC, HIGH);
+			digitalWrite(segD, HIGH);
+			digitalWrite(segG, HIGH);
+			delay(5);
+            break;
+
+        case 4:
+            digitalWrite(segB, HIGH);
+			digitalWrite(segC, HIGH);
+			digitalWrite(segF, HIGH);
+			digitalWrite(segG, HIGH);
+			delay(5);
+            break;
+
+        case 5:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segC, HIGH);
+			digitalWrite(segD, HIGH);
+			digitalWrite(segF, HIGH);
+			digitalWrite(segG, HIGH);
+			delay(5);
+            break;
+
+        case 6:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segC, HIGH);
+			digitalWrite(segD, HIGH);
+			digitalWrite(segE, HIGH);
+			digitalWrite(segF, HIGH);
+			digitalWrite(segG, HIGH);
+			delay(5);
+            break;
+
+        case 7:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segB, HIGH);
+			digitalWrite(segC, HIGH);
+			delay(5);
+            break;
+
+        case 8:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segB, HIGH);
+			digitalWrite(segC, HIGH);
+			digitalWrite(segD, HIGH);
+			digitalWrite(segE, HIGH);
+			digitalWrite(segF, HIGH);
+			digitalWrite(segG, HIGH);
+			delay(5);
+            break;
+
+        case 9:
+            digitalWrite(segA, HIGH);
+			digitalWrite(segB, HIGH);
+			digitalWrite(segC, HIGH);
+			digitalWrite(segD, HIGH);
+			digitalWrite(segF, HIGH);
+			digitalWrite(segG, HIGH);
+			delay(5);
+            break;
+
+    }
+} 
 
 void offLED()
 {
